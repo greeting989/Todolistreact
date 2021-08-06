a373e4f460fa23e62373f17ca590a4fb938d5128
greetings989 <ashrivastava0812@gmail.com>
2021-08-06 05:47:02 -0700

    second

--- src/Todolist.js
+++ src/Todolist.js
@@ -31,3 +31,54 @@ class TodoList1 extends Component {
             }
         }
     }
+    deleteHandler = (id) => {
+        const { list } = this.state
+        this.setState({
+            list: list.filter((item, index) => index !== id)
+        })
+    }
+    handleAddClick=()=>{
+        const {list,text} = this.state;
+        if(list.findIndex(item=>item.name === text) === -1){
+            this.setState({
+                list: [...this.state.list, { name: this.state.text, isSelected:false , isCompleted:false , isEditing:false}],
+                text: ""
+            })
+        }    else{
+            this.setState({
+                text:''
+            })
+        }
+    }
+    
+   
+    render() {
+        const { text, list } = this.state;
+        console.log(list)
+        return (
+            <div>
+                <h1>TodoList</h1>
+                <input type="text" placeholder="Add item " value={text} onChange={this.handleChange} onKeyPress={this.HandleEnter} />
+                <button type="button" onClick={this.handleAddClick}>Add item</button>
+                {list.map((item, index) => {
+                    return (
+                        <>
+                            <p>
+                                {item.name}
+                                <button onClick={() => this.deleteHandler(index)}>
+                                    Delete</button>
+                                <button onClick={() => this.editHandler(index)}>
+                                    Edit</button>
+                            </p>
+
+                        </>
+                    )
+                })
+
+                }
+            </div>
+        )
+    }
+}
+
+export default TodoList1;
